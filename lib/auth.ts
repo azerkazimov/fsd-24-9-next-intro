bcec93fffb0ea8015d81c14020e5614436c099b7
Azer Kazimov <azer.kazimov@yahoo.com>
2026-07-30 19:32:21 +0400

    autorize

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,6 @@
 import type { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import CredentialProvider from "next-auth/providers/credentials"
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -7,6 +8,25 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
     }),
+    CredentialProvider({
+      name: "credentials",
+      credentials: {
+        name: { label: "Name", type: "text" },
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        try {
+          if (!credentials?.email || !credentials?.password) return null;
+        } catch (error) {
+          console.error(error);
+          return null;
+        }
+      }
+    })
   ],
+  session: {
+    strategy: "jwt" as const,
+  },
   secret: process.env.NEXTAUTH_SECRET,
 }
