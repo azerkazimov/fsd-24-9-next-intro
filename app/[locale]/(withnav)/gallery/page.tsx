ba7dfbf09bc1c0ef59fbb44b6f62da57aa78e741
Azer Kazimov <azer.kazimov@yahoo.com>
2026-08-30 11:51:36 +0400

    Integrate OrbitModel into Gallery page and remove it from Products page; update layout accordingly.

--- app/[locale]/(withnav)/gallery/page.tsx
+++ app/[locale]/(withnav)/gallery/page.tsx
@@ -1,7 +1,12 @@
+import OrbitModel from "@/components/models/orbit";
+
 export default function Gallery() {
     return (
         <div>
             <h1>Gallery</h1>
+            <div className="w-full h-screen relative">
+                <OrbitModel />
+            </div>
         </div>
     )
 }
--- app/[locale]/(withnav)/products/page.tsx
+++ app/[locale]/(withnav)/products/page.tsx
@@ -1,13 +1,13 @@
-import OrbitModel from "@/components/models/orbit";
+
 
 
 export default function About() {
     return (
         <div>
             {/* <h1>Product</h1> */}
-            <div className="w-full h-screen relative">
-                <OrbitModel />
-            </div>
+            {/* Show here product list from prisma db .... */}
+            {/* Your code here ... */}
+            
         </div>
     )
 }
